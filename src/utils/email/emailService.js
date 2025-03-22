bed14e51f2e4a13b4264d8ae8710944c6f73730b
temple7447 <starukido@gmail.com>
2025-03-22 21:42:40 +0100

    update email service configuration: enable secure connection for Gmail SMTP

--- src/utils/email/emailService.js
+++ src/utils/email/emailService.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false,
+  secure: true,
   auth: {
     user: config.emailUser,
     pass: config.emailPassword
